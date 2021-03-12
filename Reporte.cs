970309da2e9b943cae59ed2d47877148598419fd
Guillermo Ramirez <gramz@mesoamericana.edu.gt>
2021-03-11 22:03:00 -0600

    Se crearon las clases

---
+++ Reporte.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlquilerAutomoviles
+{
+    class Reporte
+    {
+        string nombre;
+        string placa;
+        string marca;
+        int modelo;
+        string color;
+        DateTime fechaDevolucion;
+        double total;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Placa { get => placa; set => placa = value; }
+        public string Marca { get => marca; set => marca = value; }
+        public int Modelo { get => modelo; set => modelo = value; }
+        public string Color { get => color; set => color = value; }
+        public DateTime FechaDevolucion { get => fechaDevolucion; set => fechaDevolucion = value; }
+        public double Total { get => total; set => total = value; }
+    }
+}
