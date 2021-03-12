b218ab0a2e19369a5e325a9a517d7121113ef55c
Guillermo Ramirez <gramz@mesoamericana.edu.gt>
2021-03-11 22:52:16 -0600

    Creado el formulario par ingreso de vehículos. Carga la lista de vehiculos al arrancar el programa Verifica que no hayan duplicados.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxPlaca = new System.Windows.Forms.TextBox();
+            this.comboBoxMarca = new System.Windows.Forms.ComboBox();
+            this.numericUpDownModelo = new System.Windows.Forms.NumericUpDown();
+            this.comboBoxColor = new System.Windows.Forms.ComboBox();
+            this.textBoxPrecio = new System.Windows.Forms.TextBox();
+            this.buttonGuardar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownModelo)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Placa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Marca";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Modelo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(61, 313);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Color";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(64, 391);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Precio por Kilometro";
+            // 
+            // textBoxPlaca
+            // 
+            this.textBoxPlaca.Location = new System.Drawing.Point(275, 65);
+            this.textBoxPlaca.Name = "textBoxPlaca";
+            this.textBoxPlaca.Size = new System.Drawing.Size(173, 20);
+            this.textBoxPlaca.TabIndex = 5;
+            // 
+            // comboBoxMarca
+            // 
+            this.comboBoxMarca.FormattingEnabled = true;
+            this.comboBoxMarca.Items.AddRange(new object[] {
+            "Ford",
+            "Kia",
+            "Toyota",
+            ""});
+            this.comboBoxMarca.Location = new System.Drawing.Point(275, 140);
+            this.comboBoxMarca.Name = "comboBoxMarca";
+            this.comboBoxMarca.Size = new System.Drawing.Size(184, 21);
+            this.comboBoxMarca.TabIndex = 6;
+            // 
+            // numericUpDownModelo
+            // 
+            this.numericUpDownModelo.Location = new System.Drawing.Point(275, 222);
+            this.numericUpDownModelo.Maximum = new decimal(new int[] {
+            2021,
+            0,
+            0,
+            0});
+            this.numericUpDownModelo.Minimum = new decimal(new int[] {
+            2010,
+            0,
+            0,
+            0});
+            this.numericUpDownModelo.Name = "numericUpDownModelo";
+            this.numericUpDownModelo.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownModelo.TabIndex = 7;
+            this.numericUpDownModelo.Value = new decimal(new int[] {
+            2010,
+            0,
+            0,
+            0});
+            // 
+            // comboBoxColor
+            // 
+            this.comboBoxColor.FormattingEnabled = true;
+            this.comboBoxColor.Items.AddRange(new object[] {
+            "Azul",
+            "Blanco",
+            "Rojo",
+            "Negro"});
+            this.comboBoxColor.Location = new System.Drawing.Point(275, 304);
+            this.comboBoxColor.Name = "comboBoxColor";
+            this.comboBoxColor.Size = new System.Drawing.Size(184, 21);
+            this.comboBoxColor.TabIndex = 8;
+            // 
+            // textBoxPrecio
+            // 
+            this.textBoxPrecio.Location = new System.Drawing.Point(275, 391);
+            this.textBoxPrecio.Name = "textBoxPrecio";
+            this.textBoxPrecio.Size = new System.Drawing.Size(93, 20);
+            this.textBoxPrecio.TabIndex = 9;
+            // 
+            // buttonGuardar
+            // 
+            this.buttonGuardar.Location = new System.Drawing.Point(347, 479);
+            this.buttonGuardar.Name = "buttonGuardar";
+            this.buttonGuardar.Size = new System.Drawing.Size(205, 39);
+            this.buttonGuardar.TabIndex = 10;
+            this.buttonGuardar.Text = "Guardar Vehículo";
+            this.buttonGuardar.UseVisualStyleBackColor = true;
+            this.buttonGuardar.Click += new System.EventHandler(this.buttonGuardar_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(867, 555);
+            this.Controls.Add(this.buttonGuardar);
+            this.Controls.Add(this.textBoxPrecio);
+            this.Controls.Add(this.comboBoxColor);
+            this.Controls.Add(this.numericUpDownModelo);
+            this.Controls.Add(this.comboBoxMarca);
+            this.Controls.Add(this.textBoxPlaca);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownModelo)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxPlaca;
+        private System.Windows.Forms.ComboBox comboBoxMarca;
+        private System.Windows.Forms.NumericUpDown numericUpDownModelo;
+        private System.Windows.Forms.ComboBox comboBoxColor;
+        private System.Windows.Forms.TextBox textBoxPrecio;
+        private System.Windows.Forms.Button buttonGuardar;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,95 @@ namespace AlquilerAutomoviles
 {
     public partial class Form1 : Form
     {
+        List<Alquiler> alquileres = new List<Alquiler>();
+        List<Vehiculo> vehiculos = new List<Vehiculo>();
+        List<Cliente> clientes = new List<Cliente>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Como hay que validar que no se ingresen datos 
+            //repetidos del vehiculo, es necesario cargar 
+            //los datos del archivo a las listas al arrancar
+
+            //Verificar si el archivo existe para que no de error en caso de ser la
+            //primera vez que corremos el programa
+            if (File.Exists("vehiculos.txt"))
+            {
+                FileStream stream = new FileStream("vehiculos.txt", FileMode.Open, FileAccess.Read);
+                StreamReader reader = new StreamReader(stream);
+
+                while (reader.Peek() > -1)
+                {
+                    Vehiculo vehiculo = new Vehiculo();
+                    vehiculo.Placa = reader.ReadLine();
+                    vehiculo.Marca = reader.ReadLine();
+                    vehiculo.Modelo = Convert.ToInt32(reader.ReadLine());
+                    vehiculo.Color = reader.ReadLine();
+                    vehiculo.PrecioKilometro = Convert.ToDouble(reader.ReadLine());
+
+                    vehiculos.Add(vehiculo);
+                }
+                reader.Close();
+            }
+        }
+
+
+        private void GuardarVehiculo()
+        {
+            FileStream stream = new FileStream("vehiculos.txt", FileMode.OpenOrCreate, FileAccess.Write);            
+            StreamWriter writer = new StreamWriter(stream);
+            foreach (var vehiculo in vehiculos)
+            {
+                writer.WriteLine(vehiculo.Placa);
+                writer.WriteLine(vehiculo.Marca);
+                writer.WriteLine(vehiculo.Modelo);
+                writer.WriteLine(vehiculo.Color);
+                writer.WriteLine(vehiculo.PrecioKilometro);                
+            }
+            //Cerrar el archivo
+            writer.Close();
+        }
+
+        private void buttonGuardar_Click(object sender, EventArgs e)
+        {
+
+            
+                     
+            bool existe = vehiculos.Exists(v => v.Placa == textBoxPlaca.Text);
+
+            //si existe indicarlo con un Messagebox
+            if (existe)
+            {
+                MessageBox.Show("Esa placa ya existe", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                //sino existe el vehiculo
+                // se crea un nuevo vehículo con los datos del formulario
+                Vehiculo vehiculo = new Vehiculo();
+
+                vehiculo.Placa = textBoxPlaca.Text;
+                vehiculo.Marca = comboBoxMarca.SelectedItem.ToString();
+                vehiculo.Modelo = Convert.ToInt32(numericUpDownModelo.Value);
+                vehiculo.Color = comboBoxColor.SelectedItem.ToString();
+                vehiculo.PrecioKilometro = Convert.ToDouble(textBoxPrecio.Text);
+
+                //se agrega el vehiculo a la lista de vehiculos
+                vehiculos.Add(vehiculo);
+
+                //se manda a guardar la lista al archivo 
+                //para que ambos esten siempre sincronizados
+                GuardarVehiculo();
+
+            }
+
+            
+
         }
     }
 }
